0ef7bb5501f7a21bc303164631890c8bde453d20
Krishanathep Jareanpong <krishanathep@gmail.com>
2020-10-19 17:23:32 +0700

    Updates

---
+++ precache-manifest.53646b92cbcb25873eba4f65698118fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0751a2ab64876905bc33ba469e36386",
+    "url": "/react-digital-marketing-template/index.html"
+  },
+  {
+    "revision": "6d26f1c0d6c314de1d02",
+    "url": "/react-digital-marketing-template/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4b4ff007415874a80c84",
+    "url": "/react-digital-marketing-template/static/js/2.b20debce.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-digital-marketing-template/static/js/2.b20debce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d26f1c0d6c314de1d02",
+    "url": "/react-digital-marketing-template/static/js/main.7e51c074.chunk.js"
+  },
+  {
+    "revision": "6219b67ec81f4d2cedcd",
+    "url": "/react-digital-marketing-template/static/js/runtime-main.8cfa466b.js"
+  }
+]);
